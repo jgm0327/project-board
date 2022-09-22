6504da3f1f8e65d5c396d96346a8b042ab134e72
jgm0327 <gmjang6349@gmail.com>
2022-09-22 17:41:20 +0900

    #1 - Data rest 설정
    게시글 댓글의 json api 를 자동으로 만들도록 설정

--- project-board/src/main/java/com/jgm/projectboard/repository/ArticleRepository.java
+++ project-board/src/main/java/com/jgm/projectboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.jgm.projectboard.repository;
 
 import com.jgm.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
